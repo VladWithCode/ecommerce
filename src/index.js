ec745629ec17f2052573f52312b67f237bb67a93
vladwithcode <vladwithcode@gmail.com>
2022-02-03 02:22:56 -0600

    Added Routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/main.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import NotFound from './components/404';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
