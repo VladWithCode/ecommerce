fce23125c3a84c17339099f605ac653746db67e9
vladwithcode <vladwithcode@gmail.com>
2022-02-03 02:23:43 -0600

    Started Components

---
+++ src/components/NavNavigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useCategories } from '../hooks/useCategories';
+import NavLink from './NavLink';
+
+function NavNavigation() {
+  const { categories, error, loading } = useCategories();
+
+  if (error) return null;
+
+  return (
+    <div className='navbar__navigation'>
+      {loading || !categories?.length > 0 ? (
+        <p className='navbar__link'>Cargando...</p>
+      ) : (
+        categories.map(ctg => <NavLink key={ctg} to={ctg} keystr={ctg} />)
+      )}
+    </div>
+  );
+}
+
+export default NavNavigation;
