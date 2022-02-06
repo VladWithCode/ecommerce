70f78a41f7209a244116b156ee8a71430741f5a8
vladwithcode <vladwithcode@gmail.com>
2022-02-05 21:32:48 -0600

    Changed Navbar dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { SWRConfig } from 'swr';
-import NavBar from './components/NavBar';
+import NavBar from './components/Navbar/NavBar';
+import { SWR_REFRESH_INTERVAL } from './config/globals';
 import { swrFetcher } from './functions/serverRequest';
 
 export default function App() {
   return (
     <SWRConfig
       value={{
-        refreshInterval: 5000,
+        refreshInterval: SWR_REFRESH_INTERVAL,
         fetcher: swrFetcher,
       }}>
       <div className='App'>
