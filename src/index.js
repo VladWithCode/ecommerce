8a624c1dffd57c9fe5b6e642ebcc0b84cc23911e
vladwithcode <vladwithcode@gmail.com>
2022-02-05 22:12:56 -0600

    Redux Provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './sass/main.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import NotFound from './components/404';
+import { store } from './store/store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />}>
-        <Route path='*' element={<NotFound />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
