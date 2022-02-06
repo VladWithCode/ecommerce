edcef1b5e40b0fb595b7448cf55ca0309d01c3ef
vladwithcode <vladwithcode@gmail.com>
2022-02-05 22:15:18 -0600

    Redux uiActions

--- src/actions/uiActions.js
+++ src/actions/uiActions.js
@@ -1,15 +1,26 @@
 import { uiTypes } from '../types/uiTypes';
 
-export const uiShowToast = () => ({
+export const uiShowToast = (msg, isError) => ({
   type: uiTypes.uiShowToast,
+  payload: { msg, isError },
 });
 
 export const uiHideToast = () => ({
   type: uiTypes.uiHideToast,
 });
 
-export const uiShowModal = () => ({
+export const uiTempToast = (msg, isError) => {
+  return dispatch => {
+    dispatch(uiShowToast(msg, isError));
+    setTimeout(() => {
+      dispatch(uiHideToast());
+    }, 3000);
+  };
+};
+
+export const uiShowModal = orderId => ({
   type: uiTypes.uiShowModal,
+  payload: { orderId },
 });
 
 export const uiHideModal = () => ({
