4c696c397ecc401e95eba1b1617f8aa497cbefd7
vladwithcode <vladwithcode@gmail.com>
2022-02-05 21:32:21 -0600

    TODO: Check if working correctly

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,7 @@
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
 import useSWR from 'swr';
 
 export const useAuth = () => {
-  const dispatch = useDispatch();
-  const {} = useSelector();
-  const { data, error } = useSWR('/api/public/customers', undefined, {
-    refreshInterval: 1000 * 60 * 5,
-  });
+  const { data, error } = useSWR('/api/public/auth/check');
+
+  return { auth: !!data?.authenticated, error, loading: !data && !error };
 };
