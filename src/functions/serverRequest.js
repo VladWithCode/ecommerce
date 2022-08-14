9c62bfa96a20c4f0e452e79b33a9568aca066901
vladwithcode <vladwithcode@gmail.com>
2022-08-14 13:20:59 -0500

    Add swr fetcher

--- src/functions/serverRequest.js
+++ src/functions/serverRequest.js
@@ -1,5 +1,22 @@
-// const SERVER_URI = 'http://localhost:3030';
-const SERVER_URI = 'https://shinyandglowingnaths.com';
+const SERVER_URI = 'http://localhost:3030';
+// const SERVER_URI = 'https://shinyandglowingnaths.com';
+
+export const swrFetcher = async url => {
+  const res = await fetch(SERVER_URI + url);
+
+  if (!res.ok) {
+    const data = await res.json();
+    const e = new Error(
+      data.error || data.message || 'Error al conectarse con el servidor.'
+    );
+    e.info = data;
+    e.status = res.status;
+
+    throw e;
+  }
+
+  return res.json();
+};
 
 export const serverRequest = async (endpoint, method, body, headers) => {
   const options = {
