6b27c2393422433fb577ace2d1e6d742ae3ccd88
vladwithcode <vladwithcode@gmail.com>
2022-08-17 22:10:46 -0500

    SERVER URL Fixed

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,4 +1,6 @@
-export const SERVER_URL = 'http://localhost:3000';
+// export const SERVER_URL = 'http://localhost:3000';
+
+export const SERVER_URL = 'https://jewels.certificatex-mx-edu-gob.org';
 
 export const API_URL = SERVER_URL + '/api';
 
